8588fc4f9b8defa7a8d694cd117ef370e52e008c
Kumar Nishikant <nkantkumar@gmail.com>
2024-10-14 04:23:16 +0800

    first check

--- last-mile-payment/src/main/java/sg/npayment/ArraysSortObjectExample.java
+++ last-mile-payment/src/main/java/sg/npayment/ArraysSortObjectExample.java
@@ -1,12 +1,15 @@
 package sg.npayment;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ArraysSortObjectExample {
     public static void main(String[] args) {
         String[] names = {"John", "Alice", "Bob", "David", "Eve"};
+        int[] A  = {1,3,5};
         Arrays.sort(names);
         System.out.println("Sorted Array: " + Arrays.toString(names));
         // Output: Sorted Array: [Alice, Bob, David, Eve, John]
+        Arrays.stream(A).boxed().collect(Collectors.toList());
     }
 }
